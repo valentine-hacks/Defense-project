b245571771e3af6240dcca4b4aad3e4031e78e05
valentine <valentineonyeama18@gmail.com>
2025-09-02 22:03:09 +0100

    done

--- defense project 1.js
+++ defense project 1.js
@@ -41,3 +41,31 @@ const bgHeader = () =>{
                        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', bgHeader)
+
+function scrollToSection(sectionId) {
+      document.getElementById(sectionId).scrollIntoView({
+        behavior: "smooth"
+           });
+    }
+
+    const sr = ScrollReveal ({
+      origin: 'top',
+      distance: '60px',
+      duration: 2000,
+      delay: 300,
+    })
+
+    sr.reveal('.home__bg', {scale: 1.1, opacity: 1})
+    sr.reveal('.home__swiper', {origin:'right', distance: '300px', delay: 800})
+    sr.reveal('.home__data', {origin:'bottom', distance: '120px', delay: 1600})
+    sr.reveal('.swiper-pagination-bullet', {origin:'top', delay: 1800, opacity: 0})
+    sr.reveal('.home__button', {origin:'top', delay: 2200})
+    sr.reveal('.about__data .contact__content', {origin:'left'})
+    sr.reveal('.about__video .contact__img', {origin:'right'})
+    sr.reveal('.models__card', {interval: 100})
+    sr.reveal('.info__img', {distance: '120px'})
+    sr.reveal('.info__number', {origin: 'bottom', distance: '80px', delay: 800})
+    sr.reveal('.info__group', {interval: 100, delay: 1300})
+    sr.reveal('.footer__container')
+
+
